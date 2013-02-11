150016939d2fd7175e3bfecbfc73dccb7e1cb7cd
Benjamin Paillereau <bpaillereau@gmail.com>
2013-02-11 15:01:03 +0100

    local redirect to mobile site

--- mobile-extension-apps/src/main/java/org/exoplatform/addons/mobile/switcher/controllers/MobileSwitcherApplication.java
+++ mobile-extension-apps/src/main/java/org/exoplatform/addons/mobile/switcher/controllers/MobileSwitcherApplication.java
@@ -1,9 +1,7 @@
 package org.exoplatform.addons.mobile.switcher.controllers;
 
 import juzu.Path;
-import juzu.Response;
 import juzu.View;
-import juzu.request.RenderContext;
 import juzu.template.Template;
 
 import javax.inject.Inject;
@@ -22,7 +20,7 @@ public class MobileSwitcherApplication {
   @View
   public void index() throws IOException
   {
-    String url = portletPreferences.getValue("url", "http://localhost:8080/portal/mobile");
+    String url = portletPreferences.getValue("url", "/portal/mobile");
     indexTemplate.with().set("url", url).render();
   }
 
